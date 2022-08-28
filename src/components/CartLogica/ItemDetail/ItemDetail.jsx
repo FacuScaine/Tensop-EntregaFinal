c355cc3bc705f5801cbd6b53d947a8c853455808
FacuScaine <facusca@gmail.com>
2022-08-28 01:07:00 -0300

    asd

---
+++ src/components/CartLogica/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,24 @@
+ks
+
+function ItemDetail{ data }) {
+
+    const onAdd = (cantidad) => {
+        console.log("Compraste " + cantidad)
+    }
+
+    return (
+        <>
+            <Link to={`/detalle/${data.id}`} className="producto">
+                <img src={data.image} alt='hola' />
+                <h5>{data.title}</h5>
+            </Link>
+            <div className="producto">
+                <img src={data.image} alt={data.title} />
+                <h5>{data.title}</h5>
+                <ItemCount stock={5} onAdd={onAdd}></ItemCount>
+            </div>
+        </>
+    )
+};
+
+export default ItemDetail
